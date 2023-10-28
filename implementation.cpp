643955e02b92f0fb31ad339ed4274b92d7644882
Shreshth Paliyal <136225408+Shreshth-112@users.noreply.github.com>
2023-10-28 10:06:07 +0530

    Add files via upload

---
+++ implementation.cpp
@@ -0,0 +1,90 @@
+// IMPLIMENTING USING ARRAY
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    // constructor
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+};
+
+void insertAtHead(Node* &head, int d) {
+
+    // creation of node
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+
+}
+void insertAtTail(Node* &tail, int d) {
+
+    // creation of node
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail = temp;
+
+}
+
+void print(Node* &head) {
+    Node* temp = head;
+
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+
+}
+
+void insertAtPosition(Node* &head, int position, int d) {
+
+    if(position == 1) {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+
+    while(cnt < position-1) {
+        temp = temp -> next;
+        cnt++;
+    }
+
+    Node* nodeToInsert = new Node(d);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+
+
+}
+
+int main() {
+
+    // created a new node
+    Node* node1 = new Node(10);
+    // cout << node1 -> data <<endl;  
+    // cout << node1 -> next <<endl;  
+
+    // head ponted to node1
+    Node* head  = node1;
+    Node* tail  = node1;
+    print(head);
+
+    insertAtTail(tail, 12);
+    print(head);
+    insertAtTail(tail, 15);
+    print(head);
+
+    insertAtPosition(head, 1, 22);
+    print(head);
+    return 0;
+}
