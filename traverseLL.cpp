b8402e48ccd7e79312d85c52f1338af0d5f91cb7
Shreshth Paliyal <136225408+Shreshth-112@users.noreply.github.com>
2023-10-29 14:19:48 +0530

    Add files via upload

---
+++ traverseLL.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printLL(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    printLL(head);
+    return 0;
+}
