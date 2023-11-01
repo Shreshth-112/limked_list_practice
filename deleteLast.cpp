70e0389b065f13a62e43d85822d4244c979855a1
Shreshth Paliyal <136225408+Shreshth-112@users.noreply.github.com>
2023-11-01 14:48:16 +0530

    Add files via upload

---
+++ deleteLast.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printLL(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node *deleteLast(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    Node *curr = head;
+    
+    while (curr->next->next != NULL)
+        curr = curr->next;
+
+    delete (curr->next);
+    curr->next = NULL;
+    return head;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+
+    printLL(head);
+
+    head = deleteLast(head);
+
+    printLL(head);
+
+    return 0;
+}
