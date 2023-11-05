28dd568320476a44c3104667f321c5081197a6db
Shreshth Paliyal <136225408+Shreshth-112@users.noreply.github.com>
2023-11-05 11:26:31 +0530

    Add files via upload

---
+++ correct_implementationSCLL.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+// WITH FOR LOOP
+void printSCLL(Node *head)
+{
+    if (head == NULL)
+        return;
+    cout << head->data << " ";
+    for (Node *p = head->next; p != head; p = p->next)
+        cout << p->data << " ";
+    cout << endl;
+}
+
+// WITH DO WHILE LOOP 
+void printlist(Node *head){
+    if(head==NULL)return;
+    Node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(5);
+    head->next->next = new Node(20);
+    head->next->next->next = new Node(15);
+    head->next->next->next->next = head;
+
+    printSCLL(head);
+    printlist(head);
+
+    return 0;
+}
