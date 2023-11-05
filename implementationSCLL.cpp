0ef36292d156f013681331069949b2f3484ea663
Shreshth Paliyal <136225408+Shreshth-112@users.noreply.github.com>
2023-11-05 10:51:35 +0530

    my implementation (incorrect)

---
+++ implementationSCLL.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printSCLL(Node *head){
+    Node *temp = head;
+    // if(head == NULL) cout << NULL;
+    while(head->next != temp){
+        cout << head->data << " ";
+        head = head->next; 
+    }
+    cout << head->data;
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(5);
+    head->next->next = new Node(20);
+    head->next->next->next = new Node(15);
+    head->next->next->next->next = head;
+    // Node *head = NULL;
+    printSCLL(head);
+
+    return 0;
+}
